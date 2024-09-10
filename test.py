fc1a9d6bccf546903b14f826ed1bdea2cbd4454a
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2024-09-10 22:29:50 +0700

    data type and type check

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 print("My name is Arm", "18 years old", "I love sleeping", sep = "\n")
+
+"""
+data type 
+int = integer
+float = floating
+str = string
+bool = boolean
+none = none
+"""
+#type checking
+print(type(1))
+isinstance(1, int)
