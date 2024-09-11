ccacff801d7bc9ffd9da0c56e65d715005a7d0da
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2024-09-11 21:32:55 +0700

    day 3

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
 print("My name is Arm", "18 years old", "I love sleeping", sep = "\n")
 
-"""
-data type 
-int = integer
-float = floating
-str = string
-bool = boolean
-none = none
-"""
-#type checking
-print(type(1))
-isinstance(1, int)
+name = input("Please enter your name: ")
+print("Hello, welcome", name)
